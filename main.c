2448ed3014a8522bdc002f9c8926877d8984b9e1
PaulinaPogorzelska <62258093+PaulinaPogorzelska@users.noreply.github.com>
2020-03-26 19:28:21 +0100

    Change value of ns

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-unsigned int ns[] = { 10,100,1000,3000,5000,7000,9000,11000,13000,15000,18000,20000 };
+unsigned int ns[] = { 10,500,1000,3000,5000,7000,9000,11000,13000,15000 };
 
 void fill_increasing(int *t, unsigned int n) {
     for (unsigned int i = 0; i < n; i++)
